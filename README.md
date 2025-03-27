31d1f2d602c466272605c321577a16139c1ba9e8
Sahim Salem <sahim.salem@brainstation-23.com>
2025-03-27 15:48:47 +0600

    project initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_mini_calculator
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/brainstation23/flutter_mini_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.brainstation23.flutter_mini_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
