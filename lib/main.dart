a0dec46f4216fcff487e4707395f2ca27c601ea6
Sahim32 <sahim.salem@brainstation-23.com>
2025-03-30 17:36:48 +0600

    refactor(calculator): convert CalculatorScreen to MVVM

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mini_calculator/view/calculator_view.dart';
+import 'package:flutter_mini_calculator/ui/calculator/view_model/calculator_view_model.dart';
+import 'package:flutter_mini_calculator/ui/core/calculator_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CalculatorScreen(),
+      home: CalculatorScreen(viewModel: CalculatorViewModel()),
       debugShowCheckedModeBanner: false,
     );
   }
